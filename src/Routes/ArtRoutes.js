9c975131eaf4ac5a7ba790609b91257387c1c8e3
Levi-josh <levijoshuakelly@gmail.com>
2024-10-11 15:16:21 -0200

    updated

--- src/Routes/ArtRoutes.js
+++ src/Routes/ArtRoutes.js
@@ -3,8 +3,8 @@ const route = express.Router();
 
 const {getLatestCol,getTrendingCol,getMyNfts,getMyCollection} = require('../Controller/Collections/artController')
 
-route.route("/latest").post(getLatestCol);
-route.route("/trending").post(getTrendingCol);
+route.route("/latest").get(getLatestCol);
+route.route("/trending").get(getTrendingCol);
 route.route("/myNfts").get(getMyNfts);
 route.route("/myCols").get(getMyCollection);
 

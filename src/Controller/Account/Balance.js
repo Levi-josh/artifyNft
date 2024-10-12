187bfa0eae74705c5cd82b7f068e0c6bc25e0c3a
Levi-josh <levijoshuakelly@gmail.com>
2024-10-12 02:33:22 -0200

    updated

--- src/Controller/Account/Balance.js
+++ src/Controller/Account/Balance.js
@@ -1,14 +1,20 @@
-const Web3 = require('web3');
-const myweb3 = new Web3('https://mainnet.infura.io/v3/4ed6751f016349faa88d6c1a60806e76'); // Replace with your Ethereum node URL
+const { createWeb3HttpProvider, Eth } = require('web3');
+
+// Create HTTP provider with your Infura URL
+const provider = createWeb3HttpProvider('https://mainnet.infura.io/v3/4ed6751f016349faa88d6c1a60806e76');
+
+// Initialize Eth instance for interacting with Ethereum
+const eth = new Eth(provider); // Replace with your Ethereum node URL
 const users = require('../../Models/UserSchema')
 
 const getbalance = async (req, res) => {
     const address = req.params.address;
     try {
-        const balance = await myweb3.eth.getBalance(address);
-        const saved = await users.updateOne({walletId:address},{$set:{balance: myweb3.utils.fromWei(balance, 'ether')}})
+        const balanceWei = await eth.getBalance(address);
+        // const balanceEther = Eth.utils.fromWei(balanceWei, 'ether')
+        const saved = await users.updateOne({walletId:address},{$set:{balance:balanceWei}})
         res.status(200).json({
-            balance: myweb3.utils.fromWei(balance, 'ether') // Convert balance to ether
+            balanceWei 
         });
     } catch (error) {
         console.error('Error fetching balance:', error);

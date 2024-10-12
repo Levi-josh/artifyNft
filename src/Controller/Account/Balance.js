4a5b86577229a146faaa9d2c30132555d12ed5d7
Levi-josh <levijoshuakelly@gmail.com>
2024-10-12 02:39:48 -0200

    updated

--- src/Controller/Account/Balance.js
+++ src/Controller/Account/Balance.js
@@ -1,16 +1,16 @@
-const { createWeb3HttpProvider, Eth } = require('web3');
+const Web3 = require('web3');
 
 // Create HTTP provider with your Infura URL
-const provider = createWeb3HttpProvider('https://mainnet.infura.io/v3/4ed6751f016349faa88d6c1a60806e76');
+const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/4ed6751f016349faa88d6c1a60806e76');
 
-// Initialize Eth instance for interacting with Ethereum
-const eth = new Eth(provider); // Replace with your Ethereum node URL
+// Initialize Web3 with the provider
+const web3 = new Web3(provider);
 const users = require('../../Models/UserSchema')
 
 const getbalance = async (req, res) => {
     const address = req.params.address;
     try {
-        const balanceWei = await eth.getBalance(address);
+        const balanceWei = await web3.eth.getBalance(address);
         // const balanceEther = Eth.utils.fromWei(balanceWei, 'ether')
         const saved = await users.updateOne({walletId:address},{$set:{balance:balanceWei}})
         res.status(200).json({

0b6dd87d993e49e23b09dbbef326b46a60d17061
Levi-josh <levijoshuakelly@gmail.com>
2024-10-12 09:22:34 -0200

    updated

--- src/Routes/imgRoutes.js
+++ src/Routes/imgRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const upload = require('../Utils/Multer')
-const {postNfts,postCollectionPfp,postTrendingNfts,postTrendingCols,postLatestCols,postLatestNfts} = require("../Controller/Collections/imgController")
+const {postNfts,postCollectionPfp,postTrendingNfts,postTrendingCols,postLatestCols,postLatestNfts,postPopCols,postPhotoCols} = require("../Controller/Collections/imgController")
 
 route.route("/postCol").post(upload.single('image'),postCollectionPfp);
 route.route("/postNft").post(upload.single('image'),postNfts);
@@ -9,6 +9,8 @@ route.route("/postTrendNft").post(upload.single('image'),postTrendingNfts);
 route.route("/postTrendCol").post(upload.single('image'),postTrendingCols);
 route.route("/postLatestCol").post(upload.single('image'),postLatestCols);
 route.route("/postLatestNft").post(upload.single('image'),postLatestNfts);
+route.route("/postPopCols").post(upload.single('image'),postPopCols);
+route.route("/postPhotoCols").post(upload.single('image'),postPhotoCols);
 
 
 module.exports = route;

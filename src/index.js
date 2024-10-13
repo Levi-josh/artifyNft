0ce5887328a5694072fb95fa5ac9ef06a6707c7f
Levi-josh <levijoshuakelly@gmail.com>
2024-10-13 20:13:35 -0200

    updated

--- src/index.js
+++ src/index.js
@@ -19,7 +19,20 @@ const userRoute = require('./Routes/UserSchema')
 const cron = require('node-cron');
 const checkCollections = require('./BackgroundCheck/approved')
 
-
+App.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://art-work-khaki.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200); // Respond OK to preflight
+    }
+    
+    next();
+  });
+  
 const corsOptions = {
     origin: ['http://localhost:3000', 'https://art-work-khaki.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
